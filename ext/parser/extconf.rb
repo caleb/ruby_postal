bffff0aae260d04ebcf32e36583208a7ab2d4349
Caleb Land <caleb@land.fm>
2023-08-24 10:26:44 -0400

    Update extconf.rb

--- ext/parser/extconf.rb
+++ ext/parser/extconf.rb
@@ -1,38 +1,7 @@
 require 'mkmf'
 require 'rbconfig'
 
-LIBDIR      = RbConfig::CONFIG['libdir']
-INCLUDEDIR  = RbConfig::CONFIG['includedir']
-
-HEADER_DIRS = [
-  # First search /opt/local for homebrew
-  '/opt/homebrew/include',
-
-  # Then search /usr/local for people that installed from source
-  '/usr/local/include',
-
-  # Check the ruby install locations
-  INCLUDEDIR,
-
-  # Finally fall back to /usr
-  '/usr/include',
-]
-
-LIB_DIRS = [
-  # First search /opt/local for homebrew
-  '/opt/homebrew/lib',
-
-  # Then search /usr/local for people that installed from source
-  '/usr/local/lib',
-
-  # Check the ruby install locations
-  LIBDIR,
-
-  # Finally fall back to /usr
-  '/usr/lib',
-]
-
-dir_config('libpostal', HEADER_DIRS, LIB_DIRS)
+pkg_config("libpostal")
 
 $CFLAGS << " -std=gnu99"
 $LIBS << " -lpostal"
